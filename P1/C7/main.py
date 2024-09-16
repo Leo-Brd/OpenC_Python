0675ed37184789c66338e6a658cc54f82dfdc5e2
Leo-Brd <leobordet.pro@gmail.com>
2024-09-16 15:12:54 +0200

    finish P1C7

---
+++ P1/C7/main.py
@@ -0,0 +1,12 @@
+
+fruits = {"pomme":"rouge", "banane":"jaune", "orange":"orange"}
+
+fruits["kiwi"] = "vert"
+
+couleur_banane = fruits["banane"]
+
+fruits["pomme"] = "vert"
+
+del fruits["banane"]
+
+print(fruits.keys())
