39cd22535a96e4cc98a040f18c484fc7b7233ff2
Leo-Brd <leobordet.pro@gmail.com>
2024-09-16 15:01:18 +0200

    finish P1C5

---
+++ P1/C5/main.py
@@ -0,0 +1,15 @@
+
+nom = "jean"
+
+age = 19
+
+taille = 1.75
+
+est_etudiant = True
+
+print(f"Mon nom est {nom}, j ai {age} ans. Je fais {taille} cm et je suis etudiant ({est_etudiant})")
+
+print(type(nom))
+print(type(age))
+print(type(taille))
+print(type(est_etudiant))
