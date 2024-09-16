8dae951d1a2c8475a581251fcd0d36e6994c36cd
Leo-Brd <leobordet.pro@gmail.com>
2024-09-16 15:07:40 +0200

    finish P1C6

---
+++ P1/C6/main.py
@@ -0,0 +1,20 @@
+
+fruits = ["pomme", "banane", "orange"]
+
+fruits.append("kiwi")
+
+fruits.remove("orange")
+
+print(fruits)
+
+fruits[2] = "ananas"
+
+print(fruits)
+
+print(len(fruits))
+
+fruits.sort()
+
+print(fruits)
+
+
